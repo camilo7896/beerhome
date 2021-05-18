d11f15b587ee4c7e733efee412bc8cba400a7264
cristian <63164154+camilo7896@users.noreply.github.com>
2021-05-17 20:22:42 -0500

    Update configServer.php

--- library/configServer.php
+++ library/configServer.php
@@ -1,5 +1,5 @@
 <?php
 define("USER", "u320803725_artesanalonlin");
 define("SERVER", "localhost");
-define("BD", "u320803725_store	");
+define("BD", "u320803725_store");
 define("PASS", "Bitcoin2020");
